338a34c00f4dc80c83b0960c684239a8f5942750
mina magdy <mina.magdy@wscdev.net>
2026-02-03 21:25:49 +0200

    📦 NEW: add trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://todo.qacart.com',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
     // headless: false
   },
 
